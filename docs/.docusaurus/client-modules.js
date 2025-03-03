20c3689cc22da1f9b17dcad464a07363b1b40c5d
franRov13 <francisco.a.rovira1@gmail.com>
2025-03-03 09:49:51 -0400

    Uploaiding project

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/fran_rov13/Library/CloudStorage/OneDrive-Personal/VsCode/Web App/Blockhouse-Project/Crypto-tracker/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/fran_rov13/Library/CloudStorage/OneDrive-Personal/VsCode/Web App/Blockhouse-Project/Crypto-tracker/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/fran_rov13/Library/CloudStorage/OneDrive-Personal/VsCode/Web App/Blockhouse-Project/Crypto-tracker/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/fran_rov13/Library/CloudStorage/OneDrive-Personal/VsCode/Web App/Blockhouse-Project/Crypto-tracker/docs/src/css/custom.css"),
+];
